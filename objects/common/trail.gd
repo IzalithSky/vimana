c67d5080405d45846e0f469772bb0bbc8a2e784b
Sky <IzalithSky@users.noreply.github.com>
2025-06-02 01:34:12 +0100

    trail optimisation

--- objects/common/trail.gd
+++ objects/common/trail.gd
@@ -1,10 +1,12 @@
-#@tool
 class_name Trail extends MeshInstance3D
 
 
 var points: Array[Vector3] = []
 var widths: Array = []
 var life_points: Array[float] = []
+var _dirty: bool = false
+var old_pos: Vector3
+var node_ttl: float = -1.0
 
 @export var trail_enabled: bool = true
 @export var from_width: float = 0.5
@@ -16,9 +18,6 @@ var life_points: Array[float] = []
 @export var start_color: Color = Color(1, 1, 1, 1)
 @export var end_color: Color = Color(1, 1, 1, 0)
 
-var old_pos: Vector3
-var node_ttl: float = -1.0
-
 
 func _ready() -> void:
 	old_pos = global_transform.origin
@@ -26,9 +25,11 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	if trail_enabled and (old_pos - global_transform.origin).length() > motion_delta:
+	var moved: bool = (old_pos - global_transform.origin).length() > motion_delta
+	if trail_enabled and moved:
 		_append_point()
 		old_pos = global_transform.origin
+		_dirty = true
 	
 	var p: int = 0
 	var max_points: int = points.size()
@@ -37,51 +38,40 @@ func _process(delta: float) -> void:
 		if life_points[p] > lifespan:
 			_remove_point(p)
 			p -= 1
-			if p < 0: p = 0
-		max_points = points.size()
+			if p < 0:
+				p = 0
+			max_points = points.size()
+			_dirty = true
 		p += 1
+	
+	if points.size() == 1 and life_points[0] > lifespan:
+		_remove_point(0)
+		_dirty = true
+	
+	if points.size() >= 2:
+		_dirty = true
 	
 	if node_ttl > 0.0:
 		node_ttl -= delta
+		_dirty = true
 		if node_ttl <= 0.0 and points.is_empty():
 			queue_free()
 	
-	mesh.clear_surfaces()
-	if points.size() < 2:
+	if not _dirty or points.size() < 2:
 		return
-
-
-	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
-	for i in points.size():
-		var t: float = float(i) / (points.size() - 1.0)
-		var curr_color: Color = start_color.lerp(end_color, 1.0 - t)
-		mesh.surface_set_color(curr_color)
-		
-		var curr_width: Vector3 = widths[i][0] - pow(1.0 - t, scale_acceleration) * widths[i][1]
-		
-		if scale_texture:
-			var t0: float = motion_delta * i
-			var t1: float = motion_delta * (i + 1)
-			mesh.surface_set_uv(Vector2(t0, 0))
-			mesh.surface_add_vertex(to_local(points[i] + curr_width))
-			mesh.surface_set_uv(Vector2(t1, 1))
-			mesh.surface_add_vertex(to_local(points[i] - curr_width))
-		else:
-			var t0: float = float(i) / points.size()
-			mesh.surface_set_uv(Vector2(t0, 0))
-			mesh.surface_add_vertex(to_local(points[i] + curr_width))
-			mesh.surface_set_uv(Vector2(t, 1))
-			mesh.surface_add_vertex(to_local(points[i] - curr_width))
-	mesh.surface_end()
+	
+	_dirty = false
+	mesh.clear_surfaces()
+	_build_surface()
 
 
 func _append_point() -> void:
 	var direction: Vector3 = (global_transform.origin - old_pos).normalized()
 	rotation.y = atan2(direction.x, direction.z)
 	points.append(global_transform.origin)
-	widths.append([
-		global_transform.basis.x * from_width,
-		global_transform.basis.x * from_width - global_transform.basis.x * to_width])
+	var width_from: Vector3 = global_transform.basis.x * from_width
+	var width_to: Vector3 = global_transform.basis.x * to_width
+	widths.append([width_from, width_from - width_to])
 	life_points.append(0.0)
 
 
@@ -89,3 +79,28 @@ func _remove_point(index: int) -> void:
 	points.remove_at(index)
 	widths.remove_at(index)
 	life_points.remove_at(index)
+
+
+func _build_surface() -> void:
+	mesh.surface_begin(Mesh.PRIMITIVE_TRIANGLE_STRIP)
+	var count: int = points.size()
+	for i in count:
+		var t: float = float(i) / float(count - 1)
+		var curr_color: Color = start_color.lerp(end_color, 1.0 - t)
+		mesh.surface_set_color(curr_color)
+		var curr_width: Vector3 = widths[i][0] - pow(1.0 - t, scale_acceleration) * widths[i][1]
+	
+		if scale_texture:
+			var t0: float = motion_delta * float(i)
+			var t1: float = motion_delta * float(i + 1)
+			mesh.surface_set_uv(Vector2(t0, 0.0))
+			mesh.surface_add_vertex(to_local(points[i] + curr_width))
+			mesh.surface_set_uv(Vector2(t1, 1.0))
+			mesh.surface_add_vertex(to_local(points[i] - curr_width))
+		else:
+			var t0: float = float(i) / float(count)
+			mesh.surface_set_uv(Vector2(t0, 0.0))
+			mesh.surface_add_vertex(to_local(points[i] + curr_width))
+			mesh.surface_set_uv(Vector2(t, 1.0))
+			mesh.surface_add_vertex(to_local(points[i] - curr_width))
+	mesh.surface_end()
