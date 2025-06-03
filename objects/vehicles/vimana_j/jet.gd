a5acd2b90d40d19e35b3a0283d02a887025ca86b
Sky <IzalithSky@users.noreply.github.com>
2025-06-03 18:51:28 +0100

    tuning

--- objects/vehicles/vimana_j/jet.gd
+++ objects/vehicles/vimana_j/jet.gd
@@ -2,8 +2,8 @@ class_name Jet extends Vimana
 
 
 @export var max_thrust: float = 4000.0
-@export var max_pitch: float = 640.0
-@export var max_yaw: float = 320.0
+@export var max_pitch: float = 800.0
+@export var max_yaw: float = 800.0
 @export var max_roll: float = 120.0
 @export var lift_coefficient: float = 0.1
 @export var stall_aoa_deg: float = 45.0
